8afda521b64fb2513d619ad760f44329e9f0fa72
Nathan Mirzakandov <nmirzakandov258@gmail.com>
2019-02-23 20:08:13 +0000

    Rock Paper Scissors Game

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ imgArray[0] = new Image();
 imgArray[0].src = 'assets/rock.png';
 
 imgArray[1] = new Image();
-imgArray[1].src = 'assets/paper.png'
+imgArray[1].src = 'assets/paper.png';
 
 imgArray[2] = new Image();
-imgArray[2].src = 'assets/scissors.png'
+imgArray[2].src = 'assets/scissors.png';
 
 
 
@@ -16,18 +16,73 @@ function randomNum() {
     return randomNum;
 }
 
+
+let pScore = 0;
+let cScore = 0;
+
+
 function checkResult(userChoice) {
-    let x = document.getElementById("playerChoice")
+    let a = document.getElementById("pC");
+    let b = document.getElementById("cC");
+    let p = document.getElementById("playerScore");
+    let c = document.getElementById("computerScore");
+    let x = document.getElementById("playerChoice");
     let result = document.getElementById("result");
     let computerChoice = randomNum(); 
-    if (userChoice == computerChoice) {
+    if (userChoice == 0 && computerChoice == 0) {
+        a.innerHTML = " chose rock";
+        b.innerHTML = " chose rock";
         result.innerHTML = "You tied";
-    } else if ((userChoice == 0 && computerChoice == 1) || (userChoice == 1 && computerChoice == 2) || (userChoice == 2 && computerChoice == 0)) {
-        result.innerHTML = "You lost";
-    } else if ((userChoice == 1 && computerChoice == 0) || (userChoice == 2 && computerChoice == 1) || (userChoice == 0 && computerChoice == 2)) {
-        result.innerHTML = "You won";
+    } else if (userChoice == 1 && computerChoice == 1) {
+        a.innerHTML = " chose paper";
+        b.innerHTML = " chose paper";
+        result.innerHTML = "You tied";
+    } else if (userChoice == 2 && computerChoice == 2) {
+        a.innerHTML = " chose scissors";
+        b.innerHTML = " chose scissors";
+        result.fontcolor("brown");
+        result.innerHTML = "You tied";
+    } else if (userChoice == 0 && computerChoice == 1) {
+        a.innerHTML = " chose rock";
+        b.innerHTML = " chose paper";
+        result.innerHTML = "You lose";
+        cScore++;
+        c.innerHTML = cScore;
+    } else if (userChoice == 0 && computerChoice == 2) {
+        a.innerHTML = " chose rock";
+        b.innerHTML = " chose scissors";
+        result.innerHTML = "You win";
+        pScore++;
+        p.innerHTML = pScore;
+    } else if (userChoice == 1 && computerChoice == 0) {
+        a.innerHTML = " chose paper";
+        b.innerHTML = " chose rock";
+        result.innerHTML = "You win";
+        pScore++;
+        p.innerHTML = pScore;
+    } else if (userChoice == 1 && computerChoice == 2) {
+        a.innerHTML = " chose paper";
+        b.innerHTML = " chose scissors";
+        result.innerHTML = "You lose";
+        cScore++;
+        c.innerHTML = cScore;
+    } else if (userChoice == 2 && computerChoice == 0) {
+        a.innerHTML = " chose scissors";
+        b.innerHTML = " chose rock";
+        result.innerHTML = "You lose";
+        cScore++;
+        c.innerHTML = cScore;
+    } else if (userChoice == 2 && computerChoice == 1) {
+        a.innerHTML = " chose scissors";
+        b.innerHTML = " chose paper";
+        result.innerHTML = "You win";
+        pScore++;
+        p.innerHTML = pScore;
     }
 }
+
+
+
 
 function displayChoice(userChoice, computerChoice) {
     let user = document.getElementById("playerChoice");
